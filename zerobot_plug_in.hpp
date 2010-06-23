f155fcb33c3aaaf042b655a719445d3d2b3e5396
Martin Wegner <mw@mroot.net>
2010-06-24 01:24:28 +0200

    Add getters for plug-in priority and name.

--- zerobot_plug_in.hpp
+++ zerobot_plug_in.hpp
@@ -9,6 +9,9 @@ class ZeroBotPlugIn {
 	public:
 		ZeroBotPlugIn(unsigned int _priority, std::string const &_name);
 
+		unsigned int getPriority() const { return priority; }
+		std::string const &getName() const { return name; }
+
 	protected:
 		const unsigned int priority;
 		const std::string name;
