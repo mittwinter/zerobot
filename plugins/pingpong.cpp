c277b79ac49bdbbd6c06d23b410e0a78ee5b4b7d
Martin Wegner <mw@mroot.net>
2010-06-25 20:33:52 +0200

    Stub implementation of pingpong plug-in.

--- plugins/pingpong.cpp
+++ plugins/pingpong.cpp
@@ -1,3 +1,23 @@
 #include "pingpong.hpp"
 
+namespace zerobot {
+
+PlugInPingPong::PlugInPingPong(int _priority) : PlugIn(_priority, "pingpong") {
+}
+
+PlugInResult PlugInPingPong::onConnect(IRC::Message const &_message) {
+	return onPacket(_message);
+}
+
+PlugInResult PlugInPingPong::onPacket(IRC::Message const &_message) {
+	PlugInResult result;
+	result.newState = STATE_NOP;
+	return result;
+}
+
+PlugInResult PlugInPingPong::onDisconnect(IRC::Message const &_message) {
+	return onPacket(_message);
+}
+
+}
 
--- plugins/pingpong.hpp
+++ plugins/pingpong.hpp
@@ -1,7 +1,23 @@
 #ifndef PLUGINS_PINGPONG_HPP
 #define PLUGINS_PINGPONG_HPP
 
+#include "base.hpp"
 
+namespace zerobot {
+
+class PlugInPingPong : public PlugIn {
+	protected:
+		PlugInPingPong(int _priority);
+		virtual ~PlugInPingPong() {}
+
+		virtual PlugInResult onConnect(IRC::Message const &_message);		
+		virtual PlugInResult onPacket(IRC::Message const &_message);		
+		virtual PlugInResult onDisconnect(IRC::Message const &_message);		
+
+	protected:
+};
+
+}
 
 #endif
 
