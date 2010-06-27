3da54c7660b22cef7187abae1ca0d268c7da8b98
Martin Wegner <mw@mroot.net>
2010-06-27 11:04:06 +0200

    PlugInPingPong: Adapt to time-triggered API of plug-ins. Add active-ping functionality.

--- plugins/pingpong.cpp
+++ plugins/pingpong.cpp
@@ -1,10 +1,14 @@
+#include <iostream> // TODO: remove after debugging
 #include <typeinfo>
 
 #include "pingpong.hpp"
 
 namespace zerobot {
 
-PlugInPingPong::PlugInPingPong(int _priority) : PlugIn(_priority, "pingpong") {
+PlugInPingPong::PlugInPingPong(int _priority, std::string const &_serverName) : PlugIn(_priority, "pingpong") {
+	serverName = _serverName;
+	lastPing = time(NULL);
+	lastPong = time(NULL);
 }
 
 std::auto_ptr< PlugInResult > PlugInPingPong::onConnect(state_t _state) {
@@ -14,12 +18,36 @@ std::auto_ptr< PlugInResult > PlugInPingPong::onConnect(state_t _state) {
 std::auto_ptr< PlugInResult > PlugInPingPong::onPacket(state_t _state, IRC::Message const &_message) {
 	try {
 		IRC::MessagePing const &ping = dynamic_cast< IRC::MessagePing const & >(_message);
+		lastPing = time(NULL);
 		std::auto_ptr< PlugInResult > result(new PlugInResult);
 		result->messages.push_back(new IRC::MessagePong(ping.getServerName()));
+		return result;
+	}
+	catch(std::bad_cast e) {
+	}
+	try {
+		IRC::MessagePong const &pong = dynamic_cast< IRC::MessagePong const & >(_message);
+		lastPong = time(NULL);
 	}
 	catch(std::bad_cast e) {
 	}
 	return std::auto_ptr< PlugInResult > (NULL);
+}
+
+std::auto_ptr< PlugInResult > PlugInPingPong::onTimeTrigger(state_t _state) {
+	std::auto_ptr< PlugInResult > result(NULL);
+	if((lastPing - lastPong) > 65) {
+		std::cerr << "PlugInPingPong: Connection timeouted." << std::endl;
+		result = std::auto_ptr< PlugInResult >(new PlugInResult);
+		result->newState = STATE_DISCONNECTED; // TODO: change this to STATE_DISCONNECTING
+	}
+	else if((time(NULL) - lastPing) > 30) {
+		std::cerr << "PlugInPingPong: Last ping is more than 30 seconds in the past, pinging server ..." << std::endl;
+		lastPing = time(NULL);
+		result = std::auto_ptr< PlugInResult >(new PlugInResult);
+		result->messages.push_back(new IRC::MessagePing(serverName));
+	}
+	return result;
 }
 
 std::auto_ptr< PlugInResult > PlugInPingPong::onDisconnect(state_t _state) {
--- plugins/pingpong.hpp
+++ plugins/pingpong.hpp
@@ -1,20 +1,27 @@
 #ifndef PLUGINS_PINGPONG_HPP
 #define PLUGINS_PINGPONG_HPP
 
+#include <string>
+#include <time.h>
+
 #include "base.hpp"
 
 namespace zerobot {
 
 class PlugInPingPong : public PlugIn {
 	public:
-		PlugInPingPong(int _priority);
+		PlugInPingPong(int _priority, std::string const &_serverName);
 		virtual ~PlugInPingPong() {}
 
 		virtual std::auto_ptr< PlugInResult > onConnect(state_t _state);
 		virtual std::auto_ptr< PlugInResult > onPacket(state_t _state, IRC::Message const &_message);
+		virtual std::auto_ptr< PlugInResult > onTimeTrigger(state_t _state);
 		virtual std::auto_ptr< PlugInResult > onDisconnect(state_t _state);
 
 	protected:
+		std::string serverName;
+		time_t lastPing;
+		time_t lastPong;
 };
 
 }
