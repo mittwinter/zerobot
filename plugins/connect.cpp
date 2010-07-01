945128af007fe54ea0f5c26fe4e0ef80efb0e8a2
Martin Wegner <mw@mroot.net>
2010-07-01 22:22:37 +0200

    PlugInConnect: Check for numeric replies and react with error message and disconnecting.

--- plugins/connect.cpp
+++ plugins/connect.cpp
@@ -1,4 +1,5 @@
 #include <bits/local_lim.h> // for HOST_NAME_MAX (POSIX limit)
+#include <typeinfo>
 #include <unistd.h>
 
 #include "connect.hpp"
@@ -30,7 +31,23 @@ std::auto_ptr< PlugInResult > PlugInConnect::onConnect(state_t _state) {
 }
 
 std::auto_ptr< PlugInResult > PlugInConnect::onPacket(state_t _state, IRC::Message const &_message) {
-	return std::auto_ptr< PlugInResult >(NULL);
+	std::auto_ptr< PlugInResult > result(NULL);
+	try {
+		IRC::MessageNumericReply const &reply = dynamic_cast< IRC::MessageNumericReply const & >(_message);
+		if(connectState == STATE_CONNECT_NICK_USER_SENT
+		    && (reply.getReplyCode() == IRC::ERR_NONICKNAMEGIVEN 
+		        || reply.getReplyCode() == IRC::ERR_ERRONEUSNICKNAME
+		        || reply.getReplyCode() == IRC::ERR_NICKNAMEINUSE
+		        || reply.getReplyCode() == IRC::ERR_NICKCOLLISION
+		        || reply.getReplyCode() == IRC::ERR_ALREADYREGISTERED)) {
+			std::cerr << "PlugInConnect: " << reply << std::endl;
+			result = std::auto_ptr< PlugInResult >(new PlugInResult);
+			result->newState = STATE_DISCONNECTING;
+		}
+	}
+	catch(std::bad_cast) {
+	}
+	return result;
 }
 
 std::auto_ptr< PlugInResult > PlugInConnect::onTimeTrigger(state_t _state) {
