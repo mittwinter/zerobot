27ed355da4cb8ed3840ec2a3ebfa02248144e486
Martin Wegner <mw@mroot.net>
2010-07-13 01:54:35 +0200

    Release source code under GPLv3.

--- plugins/base.hpp
+++ plugins/base.hpp
@@ -1,3 +1,19 @@
+/*	This file is part of zerobot.
+
+	zerobot is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	zerobot is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with zerobot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ZEROBOT_PLUG_IN_HPP
 #define ZEROBOT_PLUG_IN_HPP
 
