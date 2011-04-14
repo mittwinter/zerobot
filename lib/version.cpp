c12f09bfd98391da755f6e4d3727cf826089980a
Martin Wegner <mw@mroot.net>
2011-04-14 15:34:47 +0200

    Update copyright notices in source files.

--- lib/version.cpp
+++ lib/version.cpp
@@ -2,6 +2,6 @@
 
 namespace zerobot {
 
-std::string const versionString = "zerobot 0.1.0 -- Copyright (C) 2010 Martin Wegner -- License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>";
+std::string const versionString = "zerobot 0.1.0 -- Copyright (C) 2010-2011 Martin Wegner -- License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>";
 
 }
