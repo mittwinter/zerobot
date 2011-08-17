8fb2db96c8a34f19dab58dabe1d3891a3ece64b1
Martin Wegner <mw@mroot.net>
2011-08-17 14:26:41 +0200

    Update version string.

--- lib/version.cpp
+++ lib/version.cpp
@@ -2,6 +2,6 @@
 
 namespace zerobot {
 
-std::string const versionString = "zerobot 0.1.0 -- Copyright (C) 2010-2011 Martin Wegner -- License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>";
+std::string const versionString = "zerobot v. 0.1.0-git-20110817 -- http://zerobot.de/ -- Copyright (C) 2010-2011 Martin Wegner -- License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>";
 
 }
