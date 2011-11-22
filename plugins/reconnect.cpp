43bceeccacf88837ba89ce6fe00c8a513b58d5af
Martin Wegner <mw@mroot.net>
2011-11-22 18:31:33 +0100

    Whoops, actually add reconnect plug-in.

---
+++ plugins/reconnect.cpp
@@ -0,0 +1,49 @@
+/*	This file is part of zerobot.
+	Copyright (C) 2010-2011 Martin Wegner
+
+	zerobot is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	zerobot is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with zerobot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream> // TODO: remove after debugging
+
+#include "reconnect.hpp"
+
+namespace zerobot {
+
+PlugInReconnect::PlugInReconnect( int priority ) : PlugIn( priority, "reconnect" ) {
+}
+
+std::auto_ptr< PlugInResult > PlugInReconnect::onConnect( state_t state ) {
+	return std::auto_ptr< PlugInResult >( NULL );
+}
+
+std::auto_ptr< PlugInResult > PlugInReconnect::onPacket( state_t state, IRC::Message const &message ) {
+	return std::auto_ptr< PlugInResult >( NULL );
+}
+
+std::auto_ptr< PlugInResult > PlugInReconnect::onTimeTrigger( state_t state ) {
+	return std::auto_ptr< PlugInResult >( NULL );
+}
+
+std::auto_ptr< PlugInResult > PlugInReconnect::onDisconnect( state_t state ) {
+	std::auto_ptr< PlugInResult > result( NULL );
+	if( state == STATE_DISCONNECTED ) {
+		result = std::auto_ptr< PlugInResult >( new PlugInResult );
+		result->newState = STATE_CONNECTING;
+	}
+	return result;
+}
+
+}
+
---
+++ plugins/reconnect.hpp
@@ -0,0 +1,41 @@
+/*	This file is part of zerobot.
+	Copyright (C) 2010-2011 Martin Wegner
+
+	zerobot is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	zerobot is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with zerobot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PLUGINS_RECONNECT_HPP
+#define PLUGINS_RECONNECT_HPP
+
+#include "base.hpp"
+
+namespace zerobot {
+
+class PlugInReconnect : public PlugIn {
+	public:
+		PlugInReconnect( int priority );
+		virtual ~PlugInReconnect() {}
+
+		virtual std::auto_ptr< PlugInResult > onConnect( state_t _state );
+		virtual std::auto_ptr< PlugInResult > onPacket( state_t state, IRC::Message const &message );
+		virtual std::auto_ptr< PlugInResult > onTimeTrigger( state_t state );
+		virtual std::auto_ptr< PlugInResult > onDisconnect( state_t state );
+
+	protected:
+};
+
+}
+
+#endif
+
