fcef518e965976f17924c354202acc75879c073b
Martin Wegner <mw@mroot.net>
2011-11-23 03:23:54 +0100

    Add copyleft notice to webviewer code.

--- webviewer/index.php
+++ webviewer/index.php
@@ -1,4 +1,21 @@
 <?php
+/*	This file is part of zerobot.
+	Copyright (C) 2010-2011 Martin Wegner
+
+	zerobot is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	zerobot is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with zerobot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 $path = '/path/to/zerobot.sqlite';
 $network = 'Freenode';
 $channel = '#zerobot';
